51dfb14f344281419b63aac8bfb4ef6a36549a4d
Mohammad Sahil <starlordz956@gmail.com>
2023-03-28 13:42:19 +0530

    added

---
+++ index.js
@@ -0,0 +1,13 @@
+const express= require("express")
+
+
+const app =express()
+
+app.get('/men',(req,res)=>{
+    const dataStream = fs.createReadStream('./mensdatas1.json')
+    dataStream.pipe(res)
+})
+
+app.listen(4500,()=>{
+    console.log("listening")
+})
