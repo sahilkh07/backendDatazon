137dcd47113315a22158c030f81ebf8a7487b6c4
Mohammad Sahil <starlordz956@gmail.com>
2023-04-03 12:16:17 +0530

    added

--- config/db.js
+++ config/db.js
@@ -6,6 +6,8 @@ mongoose.set('strictQuery', true);
 
 const connections = mongoose.connect("mongodb+srv://mohammadsahil90a:mohammadsahil901@cluster0.z9eg1ua.mongodb.net/amazon?retryWrites=true&w=majority")
 
+
+
 module.exports = {
     connections
 }
